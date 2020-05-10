73508c4e3ffb885346b086ae94914aab28b1d21f
Saud <saudtauqeer381@gmail.com>
2020-05-10 11:27:21 +0500

    findAll: added helper function for sorting container and nonContainer nodes. Now adding helper function to recursively flatten nested container nodes.

--- src/lib/findAll.js
+++ src/lib/findAll.js
@@ -1,5 +1,67 @@
-function findAll() {
-  console.log("find all");
+function findAll(targetNode, callback) {
+  //handle exceptions.
+  if (
+    typeof arguments[1] !== "function" &&
+    typeof arguments[1] !== typeof undefined
+  ) {
+    throw `argument callback' in function findChildren expected 'Function' but got ${typeof callback} `;
+  }
+  if (targetNode == undefined) {
+    throw `argument targetNode in function findChildren expected 'Scenenode.isContainer' scenenode but got '${typeof targetNode}' instead. Read more: https://adobexdplatform.com/plugin-docs/reference/scenegraph.html#scenenodeiscontainer--boolean`;
+  }
+  if (!targetNode.isContainer) {
+    throw `argument targetNode in function findChildren expected 'Scenenode.isContainer' scenenode but got '${targetNode.constructor.name}' instead. Read more: https://adobexdplatform.com/plugin-docs/reference/scenegraph.html#scenenodeiscontainer--boolean`;
+  }
+
+  /**
+   * Sorts a parent node into container and non-container nodes.
+   * @param {scenenode.isContainer} targetNode parent node that is going to be sorted.
+   * @returns {Object} this function returns object:
+   * {
+        containerNodes: [],
+        nonContainerNodes: [],
+      }
+   *
+   */
+  function sortContainerAndNonContainerNodes(targetNode) {
+    const children = targetNode.children;
+    //now we can use reduce on these children.
+    const iterableChildrenArray = children.map((e) => e);
+    //seperate container and non-container nodes.
+    const reducedObject = iterableChildrenArray.reduce(
+      (acc, currentNode, index) => {
+        if (currentNode.isContainer) {
+          acc.containerNodes.push(currentNode);
+        }
+        if (!currentNode.isContainer) {
+          acc.nonContainerNodes.push(currentNode);
+        }
+        return acc;
+      },
+      {
+        containerNodes: [],
+        nonContainerNodes: [],
+      }
+    );
+    const { containerNodes, nonContainerNodes } = reducedObject;
+    return { containerNodes, nonContainerNodes };
+  }
+
+  function getChildrenRecursively(targetNode) {
+    const {
+      containerNodes,
+      nonContainerNodes,
+    } = sortContainerAndNonContainerNodes(targetNode);
+    console.log(
+      containerNodes,
+      "<=====================>",
+      nonContainerNodes,
+      "last test"
+    );
+    const parent = [targetNode.children.at(0)];
+  }
+
+  getChildrenRecursively(targetNode);
 }
 
 exports.findAll = findAll;
